b34b1495136f6c78f114c07aaca9431c92f31225
Placke <cplacke@LAMU02TC0U4GTFM.uhc.com>
2019-03-23 01:16:55 -0400

    init commit

---
+++ src/services/medlink-locations.js
@@ -0,0 +1,111 @@
+//MED LINK CLINIC LOCATIONS
+
+export default [
+  {
+    name: "Camino Community Center",
+    phone: "704-596-560629",
+    address: "A133 Stetson Dr., Charlotte, NC",
+    zip: 28262,
+    url: "Linenafcclinics.org/content/bethesda-health-centerCardinal"
+  },{
+    name: "Innovations Healthcare", 
+    phone: "800-939-5911",
+    address: "B550 South Caldwell St., Suite 100, Charlotte, NC", 
+    zip: 28202,
+    url: "www.cardinalinnovations.org"
+  },{ 
+    name: "Care Ring",
+    description: "Low-Cost Clinic, Physicians Reach Out, Nurse-Family PartnershipTransit",
+    phone: "704-375-01723",
+    address: "23C601 E 5th St., Suite 140, Charlotte, NC",
+    zip: 282023,
+    url: "www.careringnc.com"
+  },{ 
+    name: "Charlotte Community Health Clinic",
+    address: "8401 Medical Plaza Dr., Suite 300, Charlotte, NC",
+    zip: 28262,
+    phone: "704-316-6561",
+    url : "www.charlottecommunityhealthclinic.org"
+  },{ 
+    name: "Charlotte Community Health Clinic",
+    address: "5301 Wilkinson Blvd., Charlotte, NC",
+    phone: "704-316-6561",
+    zip: 28208,
+    url: "www.charlottecommunityhealthclinic.org"
+  },{ 
+    name: "Airport SprinterCMC Biddle Point Family Medicine",
+    phone: "704-446-99871",
+    address: "1801 Rozzelles Ferry Rd., Charlotte, NC",
+    zip: 28208,
+    url: "www.carolinashealthcare.org"
+  },{ 
+    name: "CMC Elizabeth Family Practice",
+    phone: "704-304-7000",
+    address: "2001 Vail Ave., Charlotte, NC",
+    zip: 28207,
+    url: "www.carolinashealthcare.org"
+  },{ 
+    name: "CMC Myers Park",
+    phone: "704-446-1600",
+    address: "1350 S. Kings Dr., Charlotte, NC",
+    zip: 28207,
+    description: "OB/GYN, Pediatrics, Surgery, Internal Med/ID Orthopedics",
+    url: "www.carolinashealthcare.org"
+  },{ 
+    name: "CMC NorthPark - Family Medicine & OB/GYNP",
+    phone:"704-446-999139",
+    address: "251 Eastway Dr., Charlotte, NC",
+    zip: 28213,
+    url: "www.carolinashealthcare.org"
+  },{ 
+    name: "C.W. Williams Community Health Center",
+    phone: "704-393-77202",
+    address: "3333 Wilkinson Blvd., Charlotte, NC",
+    zip: 28208,
+    url: "www.cwwilliams.org"
+  },{ 
+    name: "AirportShifa Free Health Clinic",
+    phone: "704-706-785623T4301",
+    address: "Shamrock Dr., Charlotte, NC",
+    zip: 28215,
+    url: "www.shifahealthclinic.com",
+    description: "Open on Saturdays"
+  },{ 
+    name: "Clinic of Our Towns - Davidson",
+    phone: "704-896-0471", 
+    address: "PO Box 1842, 212 Gamble St., Davidson, NC",
+    zip: 28036,
+    url: "www.adajenkins.org"
+  },{
+    name: "Lake Norman Community Health Clinic - Huntersville",
+    phone: "704-316-6611",
+    address: "14230 Hunters Rd., Huntersville, NC",
+    zip: 28078,
+    url: "www.lnchc.org"
+  },{
+    name: "Matthews Free Medical Clinic - Matthew",
+    phoen: "704-841-8882",
+    address: "196 S. Trade St., Matthews, NC",
+    zip: 28105,
+    url: "www.matthewsfmc.org"
+  },{
+    name: "Mecklenburg County Health Department",
+    address: "2845 Beatties Ford Rd., Charlotte, NC",
+    zip: 28216,
+    phone: "704-336-47007, Appts: 704-336-65002",
+    url: "www.meckhealth.org"
+  }
+]
+
+// 26249 Billingsley Rd., Charlotte, NC 28211Info 704-336-470015 Appts 704-336-6500 Owww.meckhealth.org
+
+// Pediatric Dental Clinic1801 Rozzelles Ferry Rd., Charlotte, NC 28208 Info 704-336-47001, 7Pwww.meckhealth.orgAppts 980-314-9600
+
+// NC MedAssistP704-536-17905Q4428 Taggart Creek Rd., Suite 101, Charlotte, NC 28208www.medassist.org
+
+Free pharmacy program for uninsured residents of NCProject 658/
+
+Smith Family Wellness CenterP704-910-58109R3622 Central Avenue, Charlotte, NC 28205www.SmithFamilyWellnessCenter.com
+Shelter Health ServicesP704-334-000013, 26S534 Spratt St., Charlotte, NC 28206www.shelterhealthservices.com
+
+Services for Salvation Army Center of Hope Residents
